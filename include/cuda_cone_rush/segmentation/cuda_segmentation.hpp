5bd711fb4808a3ba138ba51e3e3d2555e2f88ebf
Leonardo Nels <81677769+leonardonels@users.noreply.github.com>
2026-03-22 22:10:45 +0100

    TODO: test refactor segmentation types and rename class
    Removed old enum definitions for SacModel and SacMethod, and updated the cudaSegmentation class to CudaSegmentation.

--- include/cuda_cone_rush/segmentation/cuda_segmentation.hpp
+++ include/cuda_cone_rush/segmentation/cuda_segmentation.hpp
@@ -6,39 +6,6 @@
 #include <cuda_runtime.h>
 #include <thrust/device_vector.h>
 
-typedef enum
-{
-    SACMODEL_PLANE = 0,
-    SACMODEL_LINE,
-    SACMODEL_CIRCLE2D,
-    SACMODEL_CIRCLE3D,
-    SACMODEL_SPHERE,
-    SACMODEL_CYLINDER,
-    SACMODEL_CONE,
-    SACMODEL_TORUS,
-    SACMODEL_PARALLEL_LINE,
-    SACMODEL_PERPENDICULAR_PLANE,
-    SACMODEL_PARALLEL_LINES,
-    SACMODEL_NORMAL_PLANE,
-    SACMODEL_NORMAL_SPHERE,
-    SACMODEL_REGISTRATION,
-    SACMODEL_REGISTRATION_2D,
-    SACMODEL_PARALLEL_PLANE,
-    SACMODEL_NORMAL_PARALLEL_PLANE,
-    SACMODEL_STICK,
-} SacModel;
-
-typedef enum
-{
-    SAC_RANSAC = 0,
-    SAC_LMEDS = 1,
-    SAC_MSAC = 2,
-    SAC_RRANSAC = 3,
-    SAC_RMSAC = 4,
-    SAC_MLESAC = 5,
-    SAC_PROSAC = 6,
-} SacMethod;
-
 typedef struct
 {
     double distanceThreshold;
@@ -47,27 +14,6 @@ typedef struct
     double maxSegmentationDistance; // new parameter to limit how far points can be from the sensor for plane fitting
     bool optimizeCoefficients;
 } segParam_t;
-
-class cudaSegmentation
-{
-public:
-    // Now Just support: SAC_RANSAC + SACMODEL_PLANE
-    cudaSegmentation(int ModelType, int MethodType, cudaStream_t stream = 0);
-
-    /*
-    Input:
-        cloud_in: data pointer for points cloud
-        nCount: count of points in cloud_in
-    Output:
-        Index: data pointer which has the index of points in a plane from input
-        modelCoefficients: data pointer which has the group of coefficients of the plane
-    */
-    int set(segParam_t param);
-    void segment(float *cloud_in, int nCount,
-                 int *index, float *modelCoefficients);
-
-private:
-};
 
 class CudaSegmentation : public Isegmentation
 {
